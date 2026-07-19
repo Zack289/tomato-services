de75a22289a55540b7c0415a8c934de66c6836e7
Zack289 <ozack911@gmail.com>
2026-07-19 14:02:44 +0545

    Ignore build and environment artifacts

--- auth/dist/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Auth sesrvice running on port http://localhost:${PORT}`);
-});
