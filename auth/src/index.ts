eb9449a69e7ccc5b7bdcf0162b6d0af89f25673b
Zack289 <ozack911@gmail.com>
2026-07-19 15:36:56 +0545

    Implement authentication service with MongoDB connection, user model, and login route

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoute from "./routes/auth.js";
+
+import connectDB from "./config/db.js";
+
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use("/api/auth", authRoute);
+
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Auth sesrvice running on port http://localhost:${PORT}`);
+  console.log(`Auth service running on port ${PORT}`);
+  connectDB();
 });
