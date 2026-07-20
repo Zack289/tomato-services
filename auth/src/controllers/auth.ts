7ab9b34778385d2e860899563e70c80a0e47b7eb
Zack289 <ozack911@gmail.com>
2026-07-20 18:27:07 +0545

    adding google api for the user crenditals

--- auth/src/controllers/auth.ts
+++ auth/src/controllers/auth.ts
@@ -3,9 +3,25 @@ import jwt from "jsonwebtoken";
 import User from "../model/user.js";
 import tryCatch from "../middlewares/trycatch.js";
 import { AuthenticatedRequest } from "../middlewares/isAuth.js";
+import { oauth2client } from "../config/googleConfig.js";
+import axios from "axios";
 
 export const loginUser = tryCatch(async (req, res) => {
-  const { email, name, picture } = req.body;
+  const { code } = req.body;
+
+  if (!code) {
+    return res.status(400).json({ message: "Authorization code is required" });
+  }
+
+  const googleResponse = await oauth2client.getToken(code);
+
+  oauth2client.setCredentials(googleResponse.tokens);
+
+  const userRes =
+    await axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleResponse.tokens.access_token}
+    `);
+
+  const { email, name, picture } = userRes.data;
 
   let user = await User.findOne({ email });
 
@@ -58,10 +74,9 @@ export const addUserRole = tryCatch(async (req: AuthenticatedRequest, res) => {
   res.json({ user, token });
 });
 
-
 //fetching the account
 
-export const myProfile = tryCatch(async(req:AuthenticatedRequest, res)=>{
+export const myProfile = tryCatch(async (req: AuthenticatedRequest, res) => {
   const user = req.user;
-  res.json(user)
-})
+  res.json(user);
+});
