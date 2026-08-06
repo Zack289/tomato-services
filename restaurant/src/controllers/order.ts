25786458ac12968b1198aec2d8a8333541e38c54
Zack289 <ozack911@gmail.com>
2026-08-06 18:13:22 +0545

    feat: implement order creation functionality; add order model and controller

---
+++ restaurant/src/controllers/order.ts
@@ -0,0 +1,140 @@
+import { AuthenticatedRequest } from "../middlewares/isAuth.js";
+import tryCatch from "../middlewares/tryCatch.js";
+import Address from "../models/Address.js";
+import cart from "../models/cart.js";
+import { IMenuItem } from "../models/menuItems.js";
+import Order from "../models/Order.js";
+import Restaurant, { IRestaurant } from "../models/Restaurant.js";
+
+export const createOrder = tryCatch(async (req: AuthenticatedRequest, res) => {
+  const user = req.user;
+
+  if (!user) {
+    return res.status(401).json({
+      message: "unauthorized",
+    });
+  }
+
+  const { paymentMethod, addressId, distance } = req.body;
+
+  if (!addressId) {
+    return res.status(400).json({
+      message: "Address is required",
+    });
+  }
+
+  const address = await Address.findOne({
+    _id: addressId,
+    userId: user._id,
+  });
+
+  if (!address) {
+    return res.status(404).json({
+      message: "Address not found",
+    });
+  }
+
+  const cartItems = await cart
+    .find({
+      userId: user._id,
+    })
+    .populate<{ itemId: IMenuItem }>("itemId")
+    .populate<{ restaurantId: IRestaurant }>("restaurantId");
+
+  if (cartItems.length === 0) {
+    return res.status(400).json({
+      message: "Cart is empty",
+    });
+  }
+
+  const firstCartItem = cartItems[0];
+
+  if (!firstCartItem || !firstCartItem.restaurantId) {
+    return res.status(400).json({
+      message: "Invalid cart data",
+    });
+  }
+
+  const restaurantId = firstCartItem.restaurantId._id;
+
+  const restaurant = await Restaurant.findById(restaurantId);
+
+  if (!restaurant) {
+    return res.status(404).json({
+      message: "No restaurant with this id",
+    });
+  }
+
+  if (!restaurant.isOpen) {
+    return res.status(404).json({
+      message: "Sorry, this restaurant is closed for now",
+    });
+  }
+
+  let subTotal = 0;
+
+  const orderItem = cartItems.map((cart) => {
+    const item = cart.itemId;
+
+    if (!item) {
+      throw new Error("Invalid cart item");
+    }
+
+    const itemTotal = item.price * cart.quantity;
+
+    subTotal += itemTotal;
+
+    return {
+      itemId: item._id.toString(),
+      name: item.name,
+      price: item.price,
+      quantity: cart.quantity,
+    };
+  });
+
+  const deliveryFee = subTotal < 250 ? 49 : 0;
+  const platformFee = 7;
+  const totalAmount = subTotal + deliveryFee + platformFee;
+
+  const expiresAt = new Date(Date.now() + 15 * 60 * 1000); // 15 minutes
+
+  const [longitude, latitude] = address.location.coordinates;
+
+  const riderAmount = Math.ceil(distance) * 17;
+
+  const order = await Order.create({
+    userId: user._id.toString(),
+    restaurantId: restaurantId.toString(),
+    restaurantName: restaurant.name,
+    riderId: null,
+    distance,
+    riderAmount,
+    items: orderItem,
+    subTotal,
+    deliveryFee,
+    platformFee,
+    totalAmount,
+    addressId: address._id.toString(),
+
+    deliveryAddress: {
+      formattedAddress: address.formattedAddress,
+      mobile: address.mobile,
+      latitude,
+      longitude,
+    },
+
+    paymentMethod,
+    paymentStatus: "pending",
+    status: "placed",
+
+    expiresAt,
+  });
+
+  await cart.deleteMany({ userId: user._id });
+
+  res.json({
+    message: "Order created successfully",
+    orderId: order._id.toString(),
+    amount: totalAmount,
+  });
+});
