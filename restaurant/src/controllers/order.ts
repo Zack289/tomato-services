f1815b0416f58e2963a4b01eba00e2e8877d7c91
Zack289 <ozack911@gmail.com>
2026-08-09 00:26:10 +0545

    feat: update createOrder to calculate distance and refactor payment key retrieval

--- restaurant/src/controllers/order.ts
+++ restaurant/src/controllers/order.ts
@@ -15,7 +15,7 @@ export const createOrder = tryCatch(async (req: AuthenticatedRequest, res) => {
     });
   }
 
-  const { paymentMethod, addressId, distance } = req.body;
+  const { paymentMethod, addressId } = req.body;
 
   if (!addressId) {
     return res.status(400).json({
@@ -33,6 +33,27 @@ export const createOrder = tryCatch(async (req: AuthenticatedRequest, res) => {
       message: "Address not found",
     });
   }
+
+  const getDistanceKm = (
+    lat1: number,
+    lon1: number,
+    lat2: number,
+    lon2: number,
+  ): number => {
+    const R = 6371;
+    const dLat = ((lat2 - lat1) * Math.PI) / 180;
+    const dLon = ((lon2 - lon1) * Math.PI) / 180;
+
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos((lat1 * Math.PI) / 180) *
+        Math.cos((lat2 * Math.PI) / 180) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return +(R * c).toFixed(2);
+  };
 
   const cartItems = await cart
     .find({
@@ -70,6 +91,13 @@ export const createOrder = tryCatch(async (req: AuthenticatedRequest, res) => {
       message: "Sorry, this restaurant is closed for now",
     });
   }
+
+  const distance = getDistanceKm(
+    address.location.coordinates[1],
+    address.location.coordinates[0],
+    restaurant.autoLocation.coordinates[1],
+    restaurant.autoLocation.coordinates[0],
+  );
 
   let subTotal = 0;
 
@@ -154,7 +182,7 @@ export const fetchOrderForPayment = tryCatch(async (req, res) => {
     });
   }
 
-  if(order.paymentStatus !== "pending"){
+  if (order.paymentStatus !== "pending") {
     return res.status(400).json({
       message: "Order already paid",
     });
@@ -163,6 +191,6 @@ export const fetchOrderForPayment = tryCatch(async (req, res) => {
   res.json({
     orderId: order._id,
     amount: order.totalAmount,
-    currency: "INR" 
-  })
+    currency: "INR",
+  });
 });
